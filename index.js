8a9083928427ca6ac86fd304c406e460b6f3cab9
martinacd08 <casandra123>
2015-05-05 22:21:15 -0700

    9

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-app.use('/',express.static(__dirname + '/'));
-
-var mysql   = require('mysql'),
+var express = require('express'),
+    app     = express(),
+    mysql   = require('mysql'),
     connectionpool = mysql.createPool({
         host     : '192.185.4.105',
         user     : 'rigarcia_proyect',
         password : 'proyecto',
         database : 'rigarcia_proyecto'
-    });
-
-
-app.get('/', function(req, res) {
-  //response.send('Hello World!');
-   connectionpool.getConnection(function(err, connection) {
+    }),
+res.setHeader({ 'Content-Type': 'application/json' });
+app.get('/:table', function(req,res){
+    connectionpool.getConnection(function(err, connection) {
         if (err) {
             console.error('CONNECTION error: ',err);
             res.statusCode = 503;
@@ -24,7 +18,7 @@ app.get('/', function(req, res) {
                 err:    err.code
             });
         } else {
-            connection.query('SELECT * FROM frac', function(err, rows, fields) {
+            connection.query('SELECT * FROM '+req.params.table, function(err, rows, fields) {
                 if (err) {
                     console.error(err);
                     res.statusCode = 500;
@@ -39,17 +33,16 @@ app.get('/', function(req, res) {
                     fields: fields,
                     json:   rows,
                     length: rows.length
-                   
                 });
                 connection.release();
             });
         }
     });
 });
-
-
-
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.get('/:table/:id', function(req,res){});
+app.post('/:table', function(req,res){});
+app.put('/:table/:id', function(req,res){});
+app.delete('/:table/:id', function(req,res){});
+ 
+app.listen(3000);
+console.log('Rest Demo Listening on port 3000');

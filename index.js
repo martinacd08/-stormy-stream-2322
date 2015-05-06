980f32a74ab038da11dfed913f78507aa52d4773
martinacd08 <casandra123>
2015-05-05 18:36:52 -0700

    5

--- index.js
+++ index.js
@@ -4,8 +4,81 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+var mysql   = require('mysql'),
+    connectionpool = mysql.createPool({
+        host     : '192.185.4.105',
+        user     : 'rigarcia_proyect',
+        password : 'proyecto',
+        database : 'rigarcia_proyecto'
+    });
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  //response.send('Hello World!');
+   connectionpool.getConnection(function(err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+                err:    err.code
+            });
+        } else {
+            connection.query('SELECT * FROM frac', function(err, rows, fields) {
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err:    err.code
+                    });
+                }
+                res.send({
+                    result: 'success',
+                    err:    '',
+                    fields: fields,
+                    json:   rows,
+                    length: rows.length
+                   
+                });
+                connection.release();
+            });
+        }
+    });
+});
+
+app.get('/fracs/"', function(req,res){
+	
+    connectionpool.getConnection(function(err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+                err:    err.code
+            });
+        } else {
+            connection.query('SELECT * FROM frac', function(err, rows, fields) {
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err:    err.code
+                    });
+                }
+                res.send({
+                    result: 'success',
+                    err:    '',
+                    fields: fields,
+                    json:   rows,
+                    length: rows.length
+                   
+                });
+                connection.release();
+            });
+        }
+    });
 });
 
 app.listen(app.get('port'), function() {
